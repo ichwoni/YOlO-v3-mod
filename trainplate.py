c6fd1f170d1c9ed6904a6c50299b972e8ea1485e
ichwoni <codnjs0397@naver.com>
2020-01-30 19:42:44 +0900

    mod

--- trainplate.py
+++ trainplate.py
@@ -23,6 +23,7 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=200, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=5, help="size of each image batch")
     parser.add_argument("--model_config_path", type=str, default="config/yolov3plate.cfg", help="path to model config file")
